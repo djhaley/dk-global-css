7a7c97de9ef8a647098297028af82c65248be444
Dan Haley <djhaley@gmail.com>
2025-03-11 20:10:18 -0700

    Set up global css element

---
+++ src/wwElement.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="css-marker" v-if="visible">CSS Loaded</div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const visible = computed(() => {
+	return wwLib && wwLib.env && wwLib.env === "development";
+});
+</script>
+
+<style>
+:root {
+	--theme-blue: #2794eb;
+	--theme-green1: #bff8d4;
+	--theme-green2: #47d6b6;
+	--theme-green3: #17b3c1;
+}
+</style>
+
+<style lang="scss" scoped>
+.css-marker {
+	position: absolute;
+	top: 0;
+	left: 0;
+	color: black;
+	background-color: var(--theme-green3) !important;
+	z-index: 100 !important;
+}
+</style>
